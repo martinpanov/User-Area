1531ecae3f744801830a8407f3bb9eaaa54d3b30
martinpanov <martin.panov98@gmail.com>
2023-10-30 14:44:07 +0200

    force footer to be on bottom, add new site button

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,22 +5,24 @@ import { AmericanExpress, MasterCard, Visa } from './FooterSVGs';
 export default function Footer() {
     return (
         <footer className={styles["footer"]}>
-            <div className={styles["footer__payment-methods-prices-wrapper"]}>
-                <div className={styles["footer__payment-methods"]}>
-                    <MasterCard />
-                    <Visa />
-                    <AmericanExpress />
+            <div className={styles["footer__wrapper"]}>
+                <div className={styles["footer__payment-methods-prices-wrapper"]}>
+                    <div className={styles["footer__payment-methods"]}>
+                        <MasterCard />
+                        <Visa />
+                        <AmericanExpress />
+                    </div>
+                    <div className={styles["footer__prices"]}>
+                        <p>Prices exclude VAT. <Link className={styles["footer__link"]} to="">Show prices with VAT</Link></p>
+                    </div>
                 </div>
-                <div className={styles["footer__prices"]}>
-                    <p>Prices exclude VAT. <Link className={styles["footer__link"]} to="">Show prices with VAT</Link></p>
-                </div>
-            </div>
-            <div className={styles["footer__copyright-wrapper"]}>
-                <span className={styles["footer__copyright"]}>
-                    Copyright &copy; 2023 SiteGround
-                </span>
-                <div className={styles["footer__link-container"]}>
-                    <Link className={styles["footer__link"]} to="">Legal</Link>
+                <div className={styles["footer__copyright-wrapper"]}>
+                    <span className={styles["footer__copyright"]}>
+                        Copyright &copy; 2023 SiteGround
+                    </span>
+                    <div className={styles["footer__link-container"]}>
+                        <Link className={styles["footer__link"]} to="">Legal</Link>
+                    </div>
                 </div>
             </div>
         </footer>
